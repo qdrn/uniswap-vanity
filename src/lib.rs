a9a13c110767b0a81213ee71de3cf3a1c3a0abd6
Adrien LF <alf@massa.net>
2024-11-25 15:43:39 +0100

    test

--- src/lib.rs
+++ src/lib.rs
@@ -597,7 +597,7 @@ pub fn gpu(config: Config) -> ocl::Result<()> {
             // let reward = rewards.get(&key).unwrap_or("0");
 
             let reward = score(address);
-            if reward > 100 {
+            if reward > 80 {
                 let output = format!(
                     "0x{}{}{} => {} => {}",
                     hex::encode(config.calling_address),
