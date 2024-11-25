843a0bb92b48f68816774bdd88486fe11c5ac3ea
Adrien LF <alf@massa.net>
2024-11-25 14:27:41 +0100

    change to nohup

--- launch.sh
+++ launch.sh
@@ -23,7 +23,7 @@ for ((i=0; i<num_sessions; i++)); do
   #   CALLER="0x34E3e542eDB4f7f4A0b41912961a7b46c972a2B4"
   #   FACTORY="0x48E516B34A1274f49457b9C6182097796D0498Cb"
   #   INIT_CODE_HASH="0x94d114296a5af85c1fd2dc039cdaa32f1ed4b0fe0868f02d888bfc91feb645d9"
-  tmux new -s $session_name -d "cargo run --release $FACTORY $CALLER $INIT_CODE_HASH $i 4 15"
+  nohup cargo run --release $FACTORY $CALLER $INIT_CODE_HASH $i 4 15 >> $session_name.log 2>&1 &
 
   echo "Tmux session '$session_name', crunching"
 done
